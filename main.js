45df0819b51abff31c28ac4a1063b0cdad537e71
sidnevski <32227121+sidnevski@users.noreply.github.com>
2018-04-23 13:41:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,138 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+//	Set environment
+//process.env.NODE_ENV = 'production';
+
+let mainWindow;
+let addWindow;
+
+//	Listen for the app to be ready
+app.on('ready', function(){
+	
+	//	Create new window
+	mainWindow = new BrowserWindow({});
+	
+	//	Load the html file into the window
+	mainWindow.loadURL(url.format({
+		
+		pathname: path.join(__dirname, 'mainWindow.html'),
+		protocol: 'file:',
+		slashes: true
+	}));
+	
+	//	Quit app when closed
+	mainWindow.on('closed', function() {
+		
+		app.quit();
+	});
+	
+	//	Build the menu from the template
+	const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+	
+	//	Insert Menu
+	Menu.setApplicationMenu(mainMenu);
+});
+
+//	Handle create window
+function createAddWindow() {
+	
+	//	Create new window
+	addWindow = new BrowserWindow({
+		
+		windth: 300,
+		height: 200,
+		title: 'Add Shopping List Item'
+	});
+	
+	//	Load the html file into the window
+	addWindow.loadURL(url.format({
+		
+		pathname: path.join(__dirname, 'addWindow.html'),
+		protocol: 'file:',
+		slashes: true
+	}));
+	
+	//	Garbage collection handleEvent
+	addWindow.on('close', function(){
+		
+		addWindow = null;
+	}); 
+}
+
+//	Catch item add
+ipcMain.on('item:add', function(e, item) {
+	
+	//console.log(item);
+	mainWindow.webContents.send('item:add', item);
+	addWindow.close();
+});
+
+//	Create menu template
+const mainMenuTemplate = [
+	{
+		
+		label: 'File',
+		submenu: [
+			{
+				
+				label: 'Add Item',
+				click() {
+					
+					createAddWindow();
+				}
+			},
+			{
+				
+				label: 'Clear Items',
+				click() {
+					
+					mainWindow.webContents.send('item:clear');
+				}
+			},
+			{
+				
+				label: 'Quit',
+				accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q', 
+				click() {
+					
+					app.quit();
+				}
+			}
+		]
+	}
+];
+
+//	If mac, add empty object to menu
+if (process.platform == 'darwin') {
+	
+	mainMenuTemplate.unshift({});
+}
+
+//	Add dev tool items if not in production
+if (process.env.NODE_ENV !== 'production') {
+	
+	mainMenuTemplate.push({
+		
+		label: 'Developer Tools',
+		submenu: [
+			
+			{
+				
+				label: 'Toggle DevTools',
+				click(item, focusedWindow) {
+					
+					focusedWindow.toggleDevTools();
+				},
+				accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I'
+			},
+			{
+				
+				role: 'reload'
+			}
+		]
+	});
+}
